4a68295ea5b0e06eec07f0f66fa83c47fec0e70a
Michał Grzędzicki <lazy404@gmail.com>
2014-09-10 16:58:51 +0200

    save progress

--- cherryBGP/webserver.py
+++ cherryBGP/webserver.py
@@ -10,24 +10,60 @@ class CherryBGPStatus(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def status(self):
-        return [{'peer':'10.0.2.2', 'status': 'Online %d' % int(time.time())}, {'peer': '10.0.2.1', 'status': 'Down'}]
+        return [{'peer':'10.0.2.2', 'status': 'Online'}, {'peer': '10.0.2.1', 'status': 'Down'}]
         
     @cherrypy.expose
     def index(self):
         return r'''<html>
     <head>
         <title>AJAX with jQuery and cherrypy</title>
+        <style type="text/css"><!--
+        body { font-family: arial, helvetica, sans-serif; font-size: 12px; font-weight: normal; color: black; background: white;}
+        th,td { font-size: 16px;}
+        h1 { font-size: x-large; margin-bottom: 0.5em;}
+        h2 { font-family: helvetica, arial; font-size: x-large; font-weight: bold; font-style: italic; color: #6020a0; margin-top: 0em; margin-bottom: 0em;}
+        h3 { font-family: helvetica, arial; font-size: 16px; font-weight: bold; color: #b00040; background: #e8e8d0; margin-top: 0em; margin-bottom: 0em;}
+        li { margin-top: 0.25em; margin-right: 2em;}
+        .hr {margin-top: 0.25em; border-color: black; border-bottom-style: solid;}
+        .titre	{background: #20D0D0;color: #000000; font-weight: bold; text-align: center;}
+        .total	{background: #20D0D0;color: #ffff80;}
+        .frontend	{background: #e8e8d0;}
+        .socket	{background: #d0d0d0;}
+        .green	{background: #c0ffc0;}
+        .red	{background: #f07849;}
+        .yellow	{background: #co7820;}
+        .rls      {letter-spacing: 0.2em; margin-right: 1px;}
+
+        a.px:link {color: #ffff40; text-decoration: none;}a.px:visited {color: #ffff40; text-decoration: none;}a.px:hover {color: #ffffff; text-decoration: none;}a.lfsb:link {color: #000000; text-decoration: none;}a.lfsb:visited {color: #000000; text-decoration: none;}a.lfsb:hover {color: #505050; text-decoration: none;}
+        table { border-collapse: collapse; border-style: none;}
+        table td { text-align: right; border-width: 1px 1px 1px 1px; border-style: solid solid solid solid; padding: 2px 3px; border-color: gray; white-space: nowrap;}
+        table td.ac { text-align: left;}
+        table td.cac { text-align: center;}
+        table th { border-width: 1px; border-style: solid solid solid solid; border-color: gray;}
+        table th.pxname { background: #b00040; color: #ffff40; font-weight: bold; border-style: solid solid none solid; padding: 2px 3px; white-space: nowrap;}
+        table th.empty { border-style: none; empty-cells: hide; background: white;}
+        table th.desc { background: white; border-style: solid solid none solid; text-align: left; padding: 2px 3px;}
+
+        table.lgd { border-collapse: collapse; border-width: 1px; border-style: none none none solid; border-color: black;}
+        table.lgd td { border-width: 1px; border-style: solid solid solid solid; border-color: gray; padding: 2px;}
+        table.lgd td.noborder { border-style: none; padding: 2px; white-space: nowrap;}
+        u {text-decoration:none; border-bottom: 1px dotted black;}
+        -->
+        </style>
         <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
         <script type="text/javascript" src="/static/json2html.js"></script>
         <script type="text/javascript" src="/static/jquery.json2html.js"></script>
     <script type="text/javascript">
         $(function() {
-        $("#testform").submit(function() {
+        $("#blform").submit(function() {
             // post the form values via AJAX…
             var postdata = {dst: $("#dst").val(), ttl: $("#ttl").val(), typ: $("#typ").val()} ;
             $.post('/routes', postdata, function(data) {
-                // and set the title with the result
-                $("#last_command").html(data['status']) ;
+                $("#last_command").html(data['log']);
+
+                if(data['status'] == 'ok')
+                    $('#blform')[0].reset();
+
                });
             return false ;
             });
@@ -65,7 +101,6 @@ class CherryBGPStatus(object):
     </head>
         <body>
         <div id="sessions">Status Unknown</div>
-        <div id="last_command"></div><br>
         Active blackhole routes:
         <table>
         <tr><th>Destination</th><th>Type</th><th>Created</th><th>Ttl</th></tr>
@@ -73,7 +108,7 @@ class CherryBGPStatus(object):
         </tbody>
         </table><br/>
         Inject blackhole:
-        <form id="testform" action="#" method="post">
+        <form id="blform" action="#" method="post">
             <p>
             <label for="dst">Name:</label>
             <input type="text" id="dst" /> <br />
@@ -92,6 +127,8 @@ class CherryBGPStatus(object):
             <input type="submit" value="Set" />
             </p>
         </form>
+
+        <div id="last_command"></div><br>
         
         </body>
     </html>'''
@@ -111,8 +148,9 @@ class CherryBGPWebService(object):
         table.append({'dst': str(dst), 'typ': str(typ), 'created': int(time.time()), 'ttl':int(ttl)})
 
         
-        return {'status': 'route %s added successfully' % dst}
-
+        return {'status': 'ok', 'log': 'route %s added successfully' % dst}
+        #return {'status': 'error', 'log': 'route %s added unsuccessfully' % dst}
+        
     def DELETE(self):
         global string
         string=""
