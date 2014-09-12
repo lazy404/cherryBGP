ae07ba50aed46896a2ed450d8db61f0051c7eeb8
Michał Grzędzicki <lazy404@gmail.com>
2014-09-12 14:17:41 +0200

    new api_call rpc

--- exabgp/cherry_exabgp.py
+++ exabgp/cherry_exabgp.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-import xmlrpclib, sys
+import xmlrpclib, sys, time
 from fcntl import fcntl, F_GETFL, F_SETFL
 from os import O_NONBLOCK, read
+from select import select
+import exceptions
 
+def api_call(cmd):
+    try:
+        junk=read(sys.stdin.fileno(), 4096)
+	#TODO: collect as status messages
+    except exceptions.OSError:
+	junk=''
 
-def stdin_read():
-    return read(sys.stdin.fileno(), 4096)
-
-def write(buf):
-    sys.stdout.write(buf)
+    sys.stdout.write(cmd)
     sys.stdout.flush()
-    return True
+    r,_,_ = select([sys.stdin], [], [], 1.0)
+    if r:
+	return read(sys.stdin.fileno(), 4096)
+    return ''
+
 
 flags = fcntl(sys.stdin, F_GETFL)
 fcntl(sys.stdin, F_SETFL, flags | O_NONBLOCK)
 
 server = SimpleXMLRPCServer(("0.0.0.0", 8000),logRequests=False)
-server.register_function(stdin_read, 'read')
-server.register_function(write, 'write')
+
+server.register_function(api_call, 'api_call')
 
 server.serve_forever()
