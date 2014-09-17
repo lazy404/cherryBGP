78efea5bf9df3a906edac48d378ad9348771a6d4
Michał Grzędzicki <lazy404@gmail.com>
2014-09-17 10:25:06 +0200

    cleanup

--- cherryBGP/util.py
+++ cherryBGP/util.py
@@ -8,37 +8,31 @@ def all_in(a, b):
     return True
 
 def nr_to_txt(cm):
-    print 'nr to txt', cm
-
     ret=[]
     for name, community in community_map.items():
         if all_in(community, cm):
             ret.append(name)
 
-    print 'nr_to_txt(%s) = %s' % (str(cm), str(ret))
-
     return ' '.join(ret)
 
 def extended(x):
-    return x.startswith('target:')
+    return x.startswith('target:') or x.startswith('origin:')
+
 def not_extended(x):
-    return not x.startswith('target:')
+    return not ( x.startswith('target:') or x.startswith('origin:') )
 
 def txt_to_nr(cm):
-    print 'txt to nr', cm
     ret=[]
     cret=Set()
     xcret=Set()
     for x in cm:
         cret.update( filter(not_extended, community_map.get(x) ))
         xcret.update( filter(extended, community_map.get(x) ))
-
-    print 'txt_to_nr(%s) = %s %s' % (str(cm), str(cret),str(xcret))
     
     if len(cret) > 0:
         ret.append('community [%s] ' % ' '.join(cret))
 
-    if len(cret) > 0:
+    if len(xcret) > 0:
         ret.append('extended-community [%s] ' % ' '.join(xcret))
 
     return ' '.join(ret)
